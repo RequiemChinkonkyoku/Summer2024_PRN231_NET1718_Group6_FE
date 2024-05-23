771905569bcab06c6578b0b6261725897389fe3f
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-05-24 02:14:50 +0700

    added asset theme files

---
+++ src/assets/theme-kit-override/components/divider.js
@@ -0,0 +1,34 @@
+import colors from "assets/theme/base/colors";
+
+import rgba from "assets/theme/functions/rgba";
+import pxToRem from "assets/theme/functions/pxToRem";
+
+const { dark, white } = colors;
+
+export default {
+  styleOverrides: {
+    root: {
+      background: rgba(dark.main, 0.2),
+      height: pxToRem(1),
+      margin: `${pxToRem(16)} 0`,
+      borderBottom: "none",
+      opacity: 0.25,
+    },
+
+    vertical: {
+      background: rgba(dark.main, 0.2),
+      width: pxToRem(1),
+      height: "100%",
+      margin: `0 ${pxToRem(16)}`,
+      borderRight: "none",
+    },
+
+    light: {
+      background: rgba(white.main, 0.2),
+
+      "&.MuiDivider-vertical": {
+        background: rgba(white.main, 0.2),
+      },
+    },
+  },
+};
---
+++ src/assets/theme-kit-override/components/popover.js
@@ -0,0 +1,19 @@
+import colors from "assets/theme/base/colors";
+import boxShadows from "assets/theme/base/boxShadows";
+import borders from "assets/theme/base/borders";
+
+const { transparent } = colors;
+const { lg } = boxShadows;
+const { borderRadius } = borders;
+
+const popover = {
+  styleOverrides: {
+    paper: {
+      backgroundColor: transparent.main,
+      boxShadow: lg,
+      borderRadius: borderRadius.md,
+    },
+  },
+};
+
+export default popover;
