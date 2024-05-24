37785810f60585b22c9e01e2a19fa8da230f22f8
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-05-24 17:52:56 +0700

    modified for testing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Sidebar from './components/Sidebar';
-import DashboardHead from'./components/DashboardHead';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
-import './assets/css/nucleo-icons.css'; // Local CSS file for icons
-import './assets/css/nucleo-svg.css'; // Local CSS file for SVGs
-import './assets/css/material-dashboard.css?v=3.1.0'; // Local CSS for material dashboard
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import DashboardHead from "./components/DashboardHead";
+import Table from "./components/Table";
+import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap CSS
+import "./assets/css/nucleo-icons.css"; // Local CSS file for icons
+import "./assets/css/nucleo-svg.css"; // Local CSS file for SVGs
+import "./assets/css/material-dashboard.css"; // Local CSS for material dashboard
 
 function App() {
   return (
     <div>
-      <DashboardHead/>
-      <Sidebar/>
+      <DashboardHead />
+      <Sidebar />
+      <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        <div class="container fluid py-4">
+          <div class="row">
+            <Table />
+          </div>
+        </div>
+      </main>
     </div>
     // <div className="App">
     //   <header className="App-header">
