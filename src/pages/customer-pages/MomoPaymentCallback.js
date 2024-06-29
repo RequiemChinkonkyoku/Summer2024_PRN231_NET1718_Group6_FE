873dfadabcffca3d6403fe911c3869d4636fb87b
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-06-29 10:49:12 +0700

    fixed momo api calls

--- src/pages/customer-pages/MomoPaymentCallback.js
+++ src/pages/customer-pages/MomoPaymentCallback.js
@@ -9,70 +9,75 @@ import FooterDash from "../../components/FooterDash";
 import { Link, useLocation, useSearchParams } from "react-router-dom";
 
 const MomoPaymentCallback = () => {
+  const location = useLocation();
+  const [errorCode, setErrorCode] = React.useState("");
+  const [orderInfo, setOrderInfo] = React.useState("");
+  const [token, setToken] = useState(localStorage.getItem("token"));
 
-    const location = useLocation();
-    const [errorCode, setErrorCode] = React.useState('');
-    const [orderInfo, setOrderInfo] = React.useState('');
-    const [appId, setAppId] = React.useState('no id');
-    const [token, setToken] = useState(localStorage.getItem("token"));
+  React.useEffect(() => {
+    if (token) {
+      const params = new URLSearchParams(location.search);
 
-    React.useEffect(() => {
-        if (token) {
-            const params = new URLSearchParams(location.search);
+      const orderInfo = decodeURIComponent(params.get("orderInfo") || "");
+      setOrderInfo(orderInfo);
+      setErrorCode(params.get("errorCode") || "1");
 
-            setOrderInfo(decodeURIComponent(params.get('orderInfo') || ''));
-            setErrorCode(params.get('errorCode') || '1');
-            setAppId(orderInfo.split('AppointmentID: ')[1]);
+      if (orderInfo) {
+        const parts = orderInfo.split("AppointmentID: ");
+        const temp = parts.length > 1 ? parts[1].trim() : null;
 
-            axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-            axios
-                .post(`/Momo/payment-execute/${appId}`)
-                .then((response) => {
-                    console.log(response.data);
-                });
-        }
-    }, []);
+        console.log(parts);
+        console.log(temp);
 
-    return (
-        <div>
-            <DashboardHead />
-            <body class="g-sidenav-show  bg-gray-200">
-                <Sidebar />
-                <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
-                    <NavbarDash />
-                    <div class="container-fluid py-4">
-                        <div class="row mb-4">
-                            <div class="col-md-6">
-                                <div class="card h-100">
-                                    <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                                        <div class="bg-gradient-info shadow-info border-radius-lg pt-4 pb-3">
-                                            <h4 class="text-white text-capitalize ps-3">
-                                                Payment Status
-                                            </h4>
-                                        </div>
-                                    </div>
-                                    <div className="card-body">
-                                        {errorCode === '0' ? (
-                                            <div>
-                                                <p>Status: Success</p>
-                                                <p>Order Info: {orderInfo}</p>
-                                            </div>
-                                        ) : (
-                                            <div>
-                                                <p>Status: Unsuccessful</p>
-                                                <p>Error Code: {errorCode}</p>
-                                            </div>
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <FooterDash />
+        // setAppId(temp ? temp.toString() : "");
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+        axios.post(`/Momo/payment-execute/${temp}`).then((response) => {
+          console.log(response.data);
+        });
+      }
+    }
+  }, []);
+
+  return (
+    <div>
+      <DashboardHead />
+      <body class="g-sidenav-show  bg-gray-200">
+        <Sidebar />
+        <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+          <NavbarDash />
+          <div class="container-fluid py-4">
+            <div class="row mb-4">
+              <div class="col-md-6">
+                <div class="card h-100">
+                  <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                    <div class="bg-gradient-info shadow-info border-radius-lg pt-4 pb-3">
+                      <h4 class="text-white text-capitalize ps-3">
+                        Payment Status
+                      </h4>
                     </div>
-                </main>
-            </body>
-        </div>
-    );
+                  </div>
+                  <div className="card-body">
+                    {errorCode === "0" ? (
+                      <div>
+                        <p>Status: Success</p>
+                        <p>Order Info: {orderInfo}</p>
+                      </div>
+                    ) : (
+                      <div>
+                        <p>Status: Unsuccessful</p>
+                        <p>Error Code: {errorCode}</p>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <FooterDash />
+          </div>
+        </main>
+      </body>
+    </div>
+  );
 };
 
 export default MomoPaymentCallback;
